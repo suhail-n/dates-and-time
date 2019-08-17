6c00154105cfb4f2eaaca12b8c6e3e28b5366717
Suhail-n <suhail.nooristani@gmail.com>
2019-08-16 23:38:53 -0400

    Add files via upload

--- src/SessionBuilder/SessionBuilder.Web/Pages/Index.cshtml.cs
+++ src/SessionBuilder/SessionBuilder.Web/Pages/Index.cshtml.cs
@@ -34,11 +34,14 @@ namespace SessionBuilder.Web.Pages
             {
                 if (session.Id == currentSession.Id) continue;
 
+                // check if sessions start time is between current sessions start and end time
+                // does session x start between session Ys start & end?
                 if (session.ScheduledAt.IsBetween(currentSession.ScheduledAt,
                     currentSession.ScheduledAt.Add(currentSession.Length)))
                 {
                     return true;
                 }
+                // does session Y start between session Xs start & end?
                 else if (currentSession.ScheduledAt.IsBetween(session.ScheduledAt,
                     session.ScheduledAt.Add(session.Length)))
                 {
@@ -53,12 +56,16 @@ namespace SessionBuilder.Web.Pages
         {
             return Speaker.Sessions.Any(
                 session => session.Id != currentSession.Id &&
+                // compares each session with current session using CompareTo 
+                // method from DateTimeOffset which returns an integer indicating if a date was earlier(-1), later(1), or the same (0)
+                // Created a DateComparison enum to represent these CompareTo integers
                 session.ScheduledAt.CompareTo(currentSession.ScheduledAt) == (int)DateComparison.Earlier
             );
         }
 
         public bool DoesSpeakerHaveLaterSessions(Session currentSession)
         {
+            // same logic as DoesSpeakerHaveEarlierSessions
             return Speaker.Sessions.Any(
                 session => session.Id != currentSession.Id &&
                 session.ScheduledAt.CompareTo(currentSession.ScheduledAt) == (int)DateComparison.Later
@@ -67,18 +74,26 @@ namespace SessionBuilder.Web.Pages
 
         public TimeSpan GetTimeUntilNextSession(Session currentSession)
         {
+            // order the sessions based on ScheduledAt time
             var nextSession = Speaker.Sessions.OrderBy(session => session.ScheduledAt)
+            // select first session that comes after the current session
                 .FirstOrDefault(session => session.Id != currentSession.Id &&
                 session.ScheduledAt >= currentSession.ScheduledAt);
-
+            
+            // return minimum timespan to indicate that a session could not be found
             if (nextSession == null) return TimeSpan.MinValue;
 
+            // subtract the next session schedule time with current session end date by adding the length to the current sessions start time
             return nextSession.ScheduledAt - currentSession.ScheduledAt.Add(currentSession.Length);
         }
 
         public TimeSpan TimeSinceSubmission(Session session)
         {
             var timeSinceSubmission =
+            // using ToOffset(session.SubmittedAt.Offset) to take the current Utc time and
+            // offsetting it to match the session offset so that their timezones align to the same zone. 
+            // This will give a current time difference from the session SubmittedAt to now without worry about which timezone the time was
+            // submitted in.
                 session.SubmittedAt - DateTimeOffset.UtcNow.ToOffset(session.SubmittedAt.Offset);
 
             return timeSinceSubmission;
